ee0c810543ce7f445c3d20343f7f6be7da16b06d
Akshay Pandey <akshayinvns@gmail.com>
2021-01-12 23:10:23 +0530

    hotel-list-second commit

--- src/app/hotel/rooms/rooms.component.ts
+++ src/app/hotel/rooms/rooms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-rooms',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  hotel;
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+   
   }
 
 }
